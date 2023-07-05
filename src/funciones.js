dc0c622910fa6f4ab6e352ee3fa5575ba099eb26
fjmr <fcojavier.menayo010@gmail.com>
2023-07-06 00:23:39 +0200

    fich funciones..js sera el fichero js principal2

--- src/funciones.js
+++ src/funciones.js
@@ -40,6 +40,60 @@
     }
 
     ]
+// creación de objeto figuras
+// const ObjFiguras=()=>{
+    const objFiguras = [
+      {
+        id:1,
+        nombre:"ciruela",
+      ruta:  "../imagenes/ciruela.svg",
+      valor: 0.05
+      },{
+        id:2,
+        nombre:"cereza",
+      ruta:  "../imagenes/cereza.svg",
+      valor: 0.10 
+      },{
+        id:3,
+        nombre:"fresa",
+      ruta:  "../imagenes/fresa.svg",
+      valor: 0.20
+      },{
+        id:4,
+        nombre:"naranja",
+      ruta:  "../imagenes/naranja.svg",
+      valor: 0.5
+      },{
+        id:5,
+        nombre: "limon",
+      ruta:  "../imagenes/limon.svg",
+      valor: 1
+      },{
+        id:6,
+        nombre: "Platanos",
+      ruta:  "../imagenes/platanos.svg",
+      valor: 2
+      },{
+        id:7,
+        nombre: "sandia",
+      ruta:  "../imagenes/sandia.svg",
+      valor: 5
+      },{
+        id:8,
+        nombre: "siete",
+      ruta:  "../imagenes/siete.svg",
+      valor: 10
+      },{
+      id:9,
+      nombre: "granpremio",
+      ruta:   "../imagenes/granpremio.svg",
+      valor:  20
+    }
+  ]
+//   return( objFiguras);
+//   }
+
+
     console.log(ObjTablero[0].valor)
     console.log(Tablero[0])
     console.log(grupoFiguras[3])
@@ -48,39 +102,65 @@
 
 
 
- 
+  //recorrido base del tablero de 4 posiciones lectura de propiedades
 for(let i=0; i<=Tablero.lengh-1;i++){
     // console.log(i)
-    console.log(document.getElementById(nombreArray[i]).getAttribute("Id"))
-    document.getElementById(nombreArray[NumeroAleatorio()]).getAttribute("Id");} 
-  
-function recorridoObjTablero(){
-let resultado=[]
+    console.log(document.getElementById(Tablero[i]).getAttribute("Id"))
+    document.getElementById(Tablero[NumeroAleatorio()]).getAttribute("Id");} 
+
+
+//funcion de recorrido de tablero
+  function recorridoObjTablero(){
+let resultadoTab=[]
 for ( let i=0; i<=ObjTablero.length-1; i++){
     let valor=NumeroAleatorio();
-    resultado.push(valor);
-    console.log(resultado[i])
+    resultadoTab.push(valor);
+    // console.log(resultadoTab[i])
 }
-return resultado;
+return resultadoTab;
 }
+
+
+
+
 let valorTablero=recorridoObjTablero()
 console.log(valorTablero)
 
+
+//recorrido de grupo figuras, que es un array
 for (let j=0; j<=grupoFiguras.length-1; j++){
-    console.log(grupoFiguras[NumeroAleatorio()])
-    
+    // console.log(grupoFiguras[NumeroAleatorio()])    
+    // console.log(ObjFiguras[j].nombre)
 }
+
+
+function recorridoObjFiguras(){
+    let resultadoFig=[];
+    for (let j=0; j<=objFiguras.length-1; j++){
+        let valor=objFiguras[NumeroAleatorio()].ruta;
+        console.log(valor)
+        resultadoFig.push(valor);
+        console.log(resultadoFig[j])
+    }
+    return resultadoFig;
+}
+
+console.log(recorridoObjFiguras())
+
+
+
+
 
 function miFuncion(){
     console.log("Ejecutando mi funcion repetidamente")
 }
 
+
 //ejecutar la funcion cada 2 segundos
-
-let intervalo= setInterval(recorridoObjTablero,1000);
-
-
-setTimeout(function(){
-    clearInterval(intervalo);
-
-},10000)
+let intervaloFiguras= setInterval(recorridoObjFiguras,1000);
+let intervaloTablero= setInterval(recorridoObjTablero,1000);
+//funciones parada de tiempo
+    setTimeout(function(){
+    clearInterval(intervaloFiguras);
+    clearInterval(intervaloTablero);
+    },10000)
