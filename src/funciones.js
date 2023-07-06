8f650971c733729199a92ee210fa7e6f7a01afa2
fjmr <fcojavier.menayo010@gmail.com>
2023-07-06 08:10:53 +0200

    hay esperanza con funciones.js

--- src/funciones.js
+++ src/funciones.js
@@ -10,32 +10,32 @@
     const grupoMarcos=["#imgn1", "#imgn2","#imgn3","#imgn4"]
     // array de las rutas de las imagenes protagonistas
     const grupoFiguras=[
-        "../imagenes/granpremio.svg","../imagenes/siete.svg","../imagenes/sandia.svg",
-        "../imagenes/platanos.svg","../imagenes/limon.svg","../imagenes/naranja.svg",
-        "../imagenes/fresa.svg","../imagenes/cereza.svg","../imagenes/ciruela.svg"
+        "./imagenes/granpremio.svg","./imagenes/siete.svg","./imagenes/sandia.svg",
+        "./imagenes/platanos.svg","./imagenes/limon.svg","./imagenes/naranja.svg",
+        "./imagenes/fresa.svg","./imagenes/cereza.svg","./imagenes/ciruela.svg"
         ]
 
     const Tablero=["imgn1","imgn2","imgn3","imgn4"];
     // Objeto TAblero con id, ruta, y valor
-    const ObjTablero=[
+    const objTablero=[
     {
     id:Tablero[0],
-     ruta: document.querySelector(Tablero[0].src),
+     ruta: document.getElementById(Tablero[0]).getAttribute("src"),
      valor : 1
     },
     {
     id:Tablero[1],
-    ruta: document.querySelector(Tablero[1].src),
+    ruta: document.getElementById(Tablero[1]).getAttribute("src"),
     valor : 1
     },
     {
     id:Tablero[2],
-    ruta: document.querySelector(Tablero[2].src),
+    ruta: document.getElementById(Tablero[2]).getAttribute("src"),
     valor : 1
     },
     {
     id:Tablero[3],
-    ruta: document.querySelector(Tablero[3].src),
+    ruta: document.getElementById(Tablero[3]).getAttribute("src"),
     valor : 1
     }
 
@@ -93,10 +93,11 @@
 //   return( objFiguras);
 //   }
 
-
-    console.log(ObjTablero[0].valor)
+    console.log(objTablero[3].ruta)
+    console.log(objTablero[0].valor)
     console.log(Tablero[0])
-    console.log(grupoFiguras[3])
+    console.log(grupoFiguras[NumeroAleatorio()])
+    
 
 
 
@@ -104,21 +105,23 @@
 
   //recorrido base del tablero de 4 posiciones lectura de propiedades
 for(let i=0; i<=Tablero.lengh-1;i++){
-    // console.log(i)
+    console.log(i)
     console.log(document.getElementById(Tablero[i]).getAttribute("Id"))
-    document.getElementById(Tablero[NumeroAleatorio()]).getAttribute("Id");} 
+    document.getElementById(Tablero[NumeroAleatorio()]).getAttribute("Id");
+  } 
 
-
+/**Funcion que recorre el objeto del tablero,y que tiene por parametro la propiedad del objetoTablero, para poder comparar, cambiar, etc */
 //funcion de recorrido de tablero
   function recorridoObjTablero(){
-let resultadoTab=[]
-for ( let i=0; i<=ObjTablero.length-1; i++){
-    let valor=NumeroAleatorio();
-    resultadoTab.push(valor);
+  let resultadoTab=[]
+    for ( let i=0; i<=objTablero.length-1; i++){
+      let valor=NumeroAleatorio();
+      resultadoTab.push(valor);
+      
     // console.log(resultadoTab[i])
-}
-return resultadoTab;
-}
+    }
+  return resultadoTab;
+  }
 
 
 
@@ -129,7 +132,7 @@ console.log(valorTablero)
 
 //recorrido de grupo figuras, que es un array
 for (let j=0; j<=grupoFiguras.length-1; j++){
-    // console.log(grupoFiguras[NumeroAleatorio()])    
+    console.log(grupoFiguras[NumeroAleatorio()])    
     // console.log(ObjFiguras[j].nombre)
 }
 
@@ -147,6 +150,66 @@ function recorridoObjFiguras(){
 
 console.log(recorridoObjFiguras())
 
+/**Alimentacion de nuevos valores de objTablero */
+for(let i=0; i<=objTablero.length-1;i++){
+  console.log(i)
+  // objTablero[i].ruta=objFiguras[NumeroAleatorio()].ruta
+  // console.log(objTablero[i].ruta)
+}
+console.log(objTablero[0].ruta)
+/**funcion para leer el atributo src de un elemento img de html */
+function leersrcElemento(){  
+  let idObjTablero;  
+  let lecturaSrcElementoImg;
+  for(let k=0; k<=Tablero.length-1;k++){
+let elementoImg=document.getElementById(objTablero[k].id);
+console.log(elementoImg);
+    idObjTablero=objTablero[k].id;
+    console.log(idObjTablero);
+    lecturaSrcElementoImg=elementoImg.getAttribute("src");
+    console.log(lecturaSrcElementoImg);
+  }
+  return (lecturaSrcElementoImg)//la funcion devuelve el valor del atrributo src del elemento solicitado
+}
+/*fin de la funcion leerSrcElemento */
+
+
+console.log(leersrcElemento())
+/** recorrido para vaciar el atributo src del elemento img de html */
+for (let k=0; k<=Tablero.length-1;k){
+  let elementoImg=document.getElementById(objTablero[k].id);
+console.log(elementoImg)
+  elementoImg.src="/imagenes/siete.svg";
+  console.log(elementoImg.src)
+}
+
+// recorrido
+for(let k=0; k<=Tablero.length-1;k++){
+  let idObjTablero=objTablero[k].id
+  console.log(idObjTablero)
+  
+  //leemos el valor del src de tablero
+  let lecturaSrcElementoImg=document.getElementById(idObjTablero).getAttribute("src")
+  console.log(lecturaSrcElementoImg)
+  
+  //cambiamos la src del elemento img, con la id de tablero k
+  let rutaAcambiar=objFiguras[NumeroAleatorio()].ruta;
+  console.log(rutaAcambiar)
+  
+  let escrituraSrcTablero=document.getElementById(idObjTablero).setAttribute("src", '+ rutaAcambiar+')
+  console.log(escrituraSrcTablero)
+  // lectura de la id de tablero
+  let lecturaIdTablero= document.getElementById(Tablero[k]).getAttribute("Id");
+if( document.getElementById(Tablero[NumeroAleatorio()]).getAttribute("Id")==Tablero[k]){
+  // document.getElementById(Tablero[k]).setAttribute("src",recorridoObjFiguras())
+  console.log(Tablero[k])
+ 
+  console.log(lecturaIdTablero)
+  console.log(escrituraSrcTablero)
+  
+}}
+
+
 
 
 
@@ -157,10 +220,12 @@ function miFuncion(){
 
 
 //ejecutar la funcion cada 2 segundos
-let intervaloFiguras= setInterval(recorridoObjFiguras,1000);
-let intervaloTablero= setInterval(recorridoObjTablero,1000);
+let intervaloFiguras= setInterval(recorridoObjFiguras,50);
+let intervaloTablero= setInterval(recorridoObjTablero,50);
 //funciones parada de tiempo
     setTimeout(function(){
     clearInterval(intervaloFiguras);
     clearInterval(intervaloTablero);
-    },10000)
+    },1000)
+
+
